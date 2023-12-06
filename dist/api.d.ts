124697834b28126c6e4cb8f33a3eeefba4d0381a
SystemInternal <githubActions@system.com>
2023-12-06 21:32:35 +0000

    Build js client files

--- dist/api.d.ts
+++ dist/api.d.ts
@@ -3174,10 +3174,10 @@ export interface TopicNode {
     'name': string;
     /**
      *
-     * @type {string}
+     * @type {Array<string>}
      * @memberof TopicNode
      */
-    'category'?: string;
+    'semantic_types'?: Array<string>;
 }
 /**
  * User profile data input.
