fad903420dd3de3c39675524c63a1928fa9275ce
SystemInternal <githubActions@system.com>
2024-01-11 20:16:26 +0000

    Build js client files

--- dist/api.d.ts
+++ dist/api.d.ts
@@ -2705,6 +2705,24 @@ export interface StudyMetadataRDBOut {
      * @memberof StudyMetadataRDBOut
      */
     'study_metadata': Array<StudyMetadataRDB>;
+    /**
+     *
+     * @type {number}
+     * @memberof StudyMetadataRDBOut
+     */
+    'total_count': number;
+    /**
+     *
+     * @type {number}
+     * @memberof StudyMetadataRDBOut
+     */
+    'offset': number;
+    /**
+     *
+     * @type {number}
+     * @memberof StudyMetadataRDBOut
+     */
+    'limit': number;
 }
 /**
  * Study output.
