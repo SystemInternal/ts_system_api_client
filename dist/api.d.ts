badbd7feb4f4c0045931140449a16a6fa0a1aae7
SystemInternal <githubActions@system.com>
2024-05-01 14:32:30 +0000

    Build js client files

--- dist/api.d.ts
+++ dist/api.d.ts
@@ -1290,6 +1290,18 @@ export interface GraphOut {
      * @memberof GraphOut
      */
     'links': Array<TopicEdge>;
+    /**
+     *
+     * @type {number}
+     * @memberof GraphOut
+     */
+    'original_link_count'?: number;
+    /**
+     *
+     * @type {number}
+     * @memberof GraphOut
+     */
+    'original_node_count'?: number;
 }
 /**
  * GraphQL query model.
